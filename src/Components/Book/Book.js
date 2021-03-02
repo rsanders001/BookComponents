67fbe1f0c9c477f5c26f1728581ea7cc6d4cb927
Ryan <rsanders2k@gmail.com>
2021-03-01 20:54:55 -0600

    Homework

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -6,7 +6,7 @@ const Book = (props) => (
   <div className="Book">
       <hr></hr>
       <h3 id="title">{props.title}</h3>
-
+      <img src="https://rsanders9.bitlampsites.com/wpd/FinalProject/wordpress/wp-content/uploads/2020/10/thelightningthief.jpg"></img>
       <p dangerouslySetInnerHTML={{__html: props.body}}></p>
       <hr></hr>
   </div>
@@ -15,11 +15,13 @@ const Book = (props) => (
 Book.propTypes = {
     title: PropTypes.string,
     body: PropTypes.string,
+    img: PropTypes.string
 };
 
 Book.defaultProps = {
     title: "Book Title",
-    body: "Book Body"
+    body: "Book Body",
+    imgURL: "https://rsanders9.bitlampsites.com/wpd/FinalProject/wordpress/wp-content/uploads/2020/10/thelightningthief.jpg"
 
 };
 
